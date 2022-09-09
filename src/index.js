b3ac42e68676b23a0ed9ef00ebaed6cc34df7153
Lea <lea.il@hotmail.com>
2022-09-09 13:38:27 +0200

    Added forecast API

--- src/index.js
+++ src/index.js
@@ -55,9 +55,21 @@ function showCityForecast(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemp = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = `b9b01b314c8b7bd51fdfba206fbf2d6a`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   
   let forecastHTML = `<div class="row ">`;
@@ -89,7 +101,6 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
 }
-
 
 function showCurrentLocation(event) {
   event.preventDefault();
@@ -135,4 +146,3 @@ celsiusLink.addEventListener("click", displayCelsiusTemp);
 let celsiusTemp = null;
 
 searchCity("Ålesund");
-displayForecast();
